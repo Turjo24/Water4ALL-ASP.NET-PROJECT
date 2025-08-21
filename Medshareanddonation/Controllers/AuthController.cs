76f4793d59bca517fd40635996df39d656371f34
kalvin-chakma <kalvinchakma7@gmail.com>
2025-08-22 04:38:08 +0600

    add: Added signin singup ui

---
+++ Medshareanddonation/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AuthController : Controller
+{
+    public IActionResult SignIn()
+    {
+        return View(); // Will look for Views/Auth/SignIn.cshtml
+    }
+
+    public IActionResult SignUp()
+    {
+        return View(); // Will look for Views/Auth/SignUp.cshtml
+    }
+}
