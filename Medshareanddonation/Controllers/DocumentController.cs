1c0b809009de85f18644938c5a5a411398ffb047
kalvin-chakma <kalvinchakma7@gmail.com>
2025-09-08 14:24:21 +0600

    add: Added Document Section and Enhanced UI

---
+++ Medshareanddonation/Controllers/DocumentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medshareanddonation.Controllers
+{
+    [Route("Document/{section?}")]
+    public class DocumentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string section = "Boiling")
+        {
+            // Detect if AJAX request (fetch with header X-Requested-With)
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return GetPartialView(section);
+            }
+
+            // Return full page with sidebar, navbar, and JavaScript
+            ViewData["Section"] = section;
+            return View();
+        }
+
+        private IActionResult GetPartialView(string section)
+        {
+            return section switch
+            {
+                "Boiling" => PartialView("_Boiling"),
+                "Filtration" => PartialView("_Filtration"),
+                "ChemicalTreatment" => PartialView("_Chemical"),
+                "UVPurification" => PartialView("_UV"),
+                _ => PartialView("_Boiling"),
+            };
+        }
+    }
+}
